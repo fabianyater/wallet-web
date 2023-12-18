9d16a4f638d4d266fa02726ec1db61c7cc12bb30
Fabian Yate <fabianyate31@gmail.com>
2023-12-18 14:57:57 -0500

    Enhance auth and data fetching setup
    Implemented React Query and authentication context to the application. Integrated AuthProvider to manage authentication state and encapsulated protected routes within PrivateRoute components. Added a new private page for testing authenticated access. These enhancements facilitate data fetching with better cache management and enforce authentication checks across private routes, improving the overall security and user experience.
    
    Refs #12345

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import { ThemeProvider } from './context/ThemeProvider';
 import './index.css';
 import LoginPage from './ui/pages/public/LoginPage';
 import SignUpPage from './ui/pages/public/SignUpPage/SignUpPage';
+import PrivateRoute from './ui/pages/components/PrivateRoute/PrivateRoute';
+import TestPage from './ui/pages/private/test';
+import { AuthProvider } from './context/AuthProvider';
+
+
+const queryClient = new QueryClient();
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<App />}>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="*" element={<h1>404</h1>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient} >
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<App />}>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/overview" element={<PrivateRoute>
+                  <TestPage />
+                </PrivateRoute>} />
+                <Route path="*" element={<h1>404</h1>} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
