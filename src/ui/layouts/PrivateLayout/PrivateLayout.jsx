cff41a8efa9df0e401cc2c17a08f5fbb87ef4551
Fabian Yate <fabianyate31@gmail.com>
2023-12-18 17:03:31 -0500

    Enhance PrivateLayout with Sidebar and Styles
    Updated the PrivateLayout component to include a Sidebar and Header, encapsulating it within a SidebarProvider. Adopted CSS Modules for improved style encapsulation with the addition of `styles.module.css`, replacing the previous `styles.css`. New grid layout provides a responsive design that adjusts content arrangement based on screen width, enhancing user navigation and overall UX on diverse devices.
    
    Resolves layout and styling inconsistencies on smaller screens.

--- src/ui/layouts/PrivateLayout/PrivateLayout.jsx
+++ src/ui/layouts/PrivateLayout/PrivateLayout.jsx
@@ -1,5 +1,17 @@
+import { SidebarProvider } from "../../../context/SidebarProvider";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import styles from "./styles.module.css";
 const PrivateLayout = ({ children }) => {
-  return <div className="holaa">{children}</div>;
+  return (
+    <SidebarProvider>
+      <div className={styles.privateLayout}>
+        <Sidebar />
+        <Header />
+        <main className={styles.content}>{children}</main>
+      </div>
+    </SidebarProvider>
+  );
 };
 
 export default PrivateLayout;
