99c0de3e77caa690b6d499838bc2c281b1923dea
Fabian Yate <fabianyate31@gmail.com>
2023-12-18 23:20:38 -0500

    Optimized layout loading with Suspense
    Implemented code splitting for PrivateLayout and PublicLayout components using the React lazy function, wrapping them in a Suspense component with a Spinner as a fallback. This change introduces a performance improvement by reducing the initial payload and speeding up the app's load time, especially beneficial for users on slow network conditions. The Suspense component ensures a smooth user experience by displaying a loading indicator while the layout components are being dynamically imported.
    
    Refactored authentication layout selection logic within the JSX to be more readable and eliminated the unnecessary Layout variable.
    
    #123 Optimize App Load Performance

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { useContext, useEffect } from 'react';
+import { Suspense, lazy, useContext, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import { ThemeContext } from './context/ThemeProvider';
-import PrivateLayout from './ui/layouts/PrivateLayout';
-import PublicLayout from './ui/layouts/PublicLayout';
 import AuthContext from './context/AuthProvider';
+import { ThemeContext } from './context/ThemeProvider';
+import Spinner from './ui/components/Spinner';
+
+const PrivateLayout = lazy(() => import('./ui/layouts/PrivateLayout'));
+const PublicLayout = lazy(() => import('./ui/layouts/PublicLayout'));
 
 function App() {
   const { theme } = useContext(ThemeContext);
   const { auth } = useContext(AuthContext);
-  const Layout = auth.token ? PrivateLayout : PublicLayout
 
   useEffect(() => {
     document.body.setAttribute('data-theme', theme);
@@ -17,9 +18,17 @@ function App() {
 
   return (
     <div data-theme={theme}>
-      <Layout>
-        <Outlet />
-      </Layout>
+      <Suspense fallback={<Spinner />}>
+        {auth.token ? (
+          <PrivateLayout>
+            <Outlet />
+          </PrivateLayout>
+        ) : (
+          <PublicLayout>
+            <Outlet />
+          </PublicLayout>
+        )}
+      </Suspense>
     </div>
   );
 }
