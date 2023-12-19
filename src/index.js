8e1d0e180a7e047348845a0017f2365d758ea84e
Fabian Yate <fabianyate31@gmail.com>
2023-12-19 03:44:07 -0500

    Add initial support for account creation flow
    Introduced a new `CreateFirstAccountPage` and `CreateAccountForm` component to facilitate users in setting up their first account. The form integrates with the backend using a mutation request to add new accounts and displays relevant notifications upon success or error. This enhancement aims to streamline the user onboarding process by providing a dedicated interface for account creation.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import SignUpPage from './ui/pages/public/SignUpPage/SignUpPage';
 import PrivateRoute from './ui/pages/components/PrivateRoute/PrivateRoute';
 import TestPage from './ui/pages/private/test';
 import { AuthProvider } from './context/AuthProvider';
+import CreateFirstAccountPage from './ui/pages/private/CreateFirstAccountPage';
 
 
 const queryClient = new QueryClient();
@@ -29,6 +30,9 @@ root.render(
                 <Route path="/signup" element={<SignUpPage />} />
                 <Route path="/overview" element={<PrivateRoute>
                   <TestPage />
+                </PrivateRoute>} />
+                <Route path="/create-first-account" element={<PrivateRoute>
+                  <CreateFirstAccountPage />
                 </PrivateRoute>} />
                 <Route path="*" element={<h1>404</h1>} />
               </Route>
