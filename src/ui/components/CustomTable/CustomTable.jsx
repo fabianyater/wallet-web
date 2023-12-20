4f674e64227efbf2937e97a6f5c3b182aa0f8edf
Fabian Yate <fabianyate31@gmail.com>
2023-12-20 14:58:42 -0500

    Enhance CustomTable with navigation on row click
    Introduced row click navigation in the CustomTable component, allowing users to be redirected to a detail page on clicking any row. This functionality is critical for improving user interaction, providing quick access to related entity details. The addition leverages React Router's `useNavigate` and enriches the table rows with click events bound to dynamic routes based on the provided `path` prop and the `walletId` from the record. As a result, user experience is streamlined within tabular data presentations, ensuring relevant information is just one click away.

--- src/ui/components/CustomTable/CustomTable.jsx
+++ src/ui/components/CustomTable/CustomTable.jsx
@@ -1,16 +1,34 @@
 import { Table } from "antd";
+import { useNavigate } from "react-router";
 
-const CustomTable = ({ isLoading, rowKey, size, footer, data, columns }) => {
+const CustomTable = ({
+  isLoading,
+  rowKey,
+  size,
+  footer,
+  data,
+  columns,
+  path,
+}) => {
+  const navigate = useNavigate();
+
   return (
     <Table
       rowKey={rowKey}
       scroll={{ x: "max-content" }}
-      loading={isLoading} 
+      loading={isLoading}
       size={size}
+      onRow={(record, rowIndex) => {
+        return {
+          onClick: () => {
+            navigate(`/${path}/${record.walletId}`, { state: { record } });
+          },
+        };
+      }}
       dataSource={data}
       columns={columns}
       pagination={{
-        position: ["bottomCenter"]
+        position: ["bottomCenter"],
       }}
       footer={() => footer}
     ></Table>
