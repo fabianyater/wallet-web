1c24589f220d3faa6e93d991a732297e3b0cd90a
Fabian Yate <fabianyate31@gmail.com>
2023-12-20 11:18:27 -0500

    Add 'CreateNewWallet' page and form export
    Introduced a new 'CreateNewWallet' component for the wallet creation process, including the creation of a form component export and the associated CSS module. This setup enables users to initiate new wallet setups within the private section of the UI, enhancing the application's functionality. The CSS module is currently placeholder and will be populated with styles in future updates.
    
    Refs #1234 (if applicable)

---
+++ src/ui/pages/private/WalletsPage/CreateNewWallet/CreateNewWallet.jsx
@@ -0,0 +1,7 @@
+import CreateWalletForm from "../../../../components/forms/CreateWalletForm";
+
+const CreateNewWallet = () => {
+  return <CreateWalletForm />;
+};
+
+export default CreateNewWallet;
