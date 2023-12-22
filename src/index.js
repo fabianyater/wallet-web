b64801e75f7667a9e7161d21511ba6cacaf2c368
Fabian Yate <fabianyate31@gmail.com>
2023-12-22 04:13:56 -0500

    Add AccountsPage and update routing
    This commit introduces the AccountsPage, including the CreateNewAccount and ListAccounts components. Additionally, routing is updated to include new paths for the account-related pages. The routing for existing WalletsPage and its subpages, such as EditWallet, has been refined too.

--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ import { AuthProvider } from './context/AuthProvider';
 import { ThemeProvider } from './context/ThemeProvider';
 import './index.css';
 import PrivateRoute from './ui/pages/components/PrivateRoute';
+import AccountsPage from './ui/pages/private/AccountsPage';
+import CreateNewAccount from './ui/pages/private/AccountsPage/CreateNewAccount';
+import ListAccounts from './ui/pages/private/AccountsPage/ListAccounts';
 import CreateFirstAccountPage from './ui/pages/private/CreateFirstAccountPage';
 import WalletsPage from './ui/pages/private/WalletsPage/';
 import CreateNewWallet from './ui/pages/private/WalletsPage/CreateNewWallet';
+import EditWallet from './ui/pages/private/WalletsPage/EditWallet';
 import ListWallets from './ui/pages/private/WalletsPage/ListWallets/';
 import TestPage from './ui/pages/private/test';
 import LoginPage from './ui/pages/public/LoginPage';
 import SignUpPage from './ui/pages/public/SignUpPage';
-import EditWallet from './ui/pages/private/WalletsPage/EditWallet';
 
 
 const queryClient = new QueryClient();
@@ -42,6 +45,10 @@ root.render(
                   <Route index element={<PrivateRoute><ListWallets /></PrivateRoute>} />
                   <Route index path='create' element={<PrivateRoute><CreateNewWallet /></PrivateRoute>} />
                   <Route index path='edit/:id' element={<PrivateRoute><EditWallet /></PrivateRoute>} />
+                </Route>
+                <Route path='accounts' element={<PrivateRoute><AccountsPage /></PrivateRoute>}>
+                  <Route index element={<PrivateRoute><ListAccounts /></PrivateRoute>} />
+                  <Route index path='create' element={<PrivateRoute><CreateNewAccount /></PrivateRoute>} />
                 </Route>
                 <Route path="*" element={<h1>404</h1>} />
               </Route>
